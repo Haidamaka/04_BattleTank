89444885270fcf8214ec8ab1286988b09bc57b04
Александр Коновал <iskander.konoval@gmail.com>
2019-01-07 20:38:33 +0200

    up to 190. Removed slippage using vector cross and dot product. Changed drive logic. C++ OnHit event.

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -2,11 +2,45 @@
 
 #include "TankTrack.h"
 
+UTankTrack::UTankTrack()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UTankTrack::BeginPlay()
+{
+	Super::BeginPlay();
+	OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
+}
+
+void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
+{
+	DriveTrack();
+	ApplySidewaysForce();
+	CurrentThrottle = 0;
+}
+
+void UTankTrack::ApplySidewaysForce()
+{
+	//work-out the required acceletation this frame to correct
+	auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+	auto DeltaTime = GetWorld()->GetDeltaSeconds();
+	auto CorrectingSpeed = -(SlippageSpeed / DeltaTime * GetRightVector());
+	//calculate and apply sideways for (F = m a )
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	auto CorrectingForce = TankRoot->GetMass() * CorrectingSpeed / 2;//two tracks
+	TankRoot->AddForce(CorrectingForce);
+}
+
 void UTankTrack::SetThrottle(float Throttle)
 {
-	
-	Throttle = FMath::Clamp<float>(Throttle, -1, +1);
-	auto ForceApplied = GetForwardVector()*Throttle*TrackMaxDrivingForce;
+	CurrentThrottle = FMath::Clamp<float>((CurrentThrottle + Throttle), -1, +1);
+}
+
+void UTankTrack::DriveTrack()
+{
+
+	auto ForceApplied = GetForwardVector()*CurrentThrottle*TrackMaxDrivingForce;
 	auto ForceLoaction = GetComponentLocation();
 	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
 	TankRoot->AddForceAtLocation(ForceApplied, ForceLoaction);
--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -14,13 +14,27 @@ class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 {
 	GENERATED_BODY()
 
+private:
+	virtual void BeginPlay() override;
+	//virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+
+	void ApplySidewaysForce();
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+
 public:
+	UTankTrack();
 	//Set Throttle between -1 and +1
 	UFUNCTION(BlueprintCallable, Category = "Controll")
 	void SetThrottle(float Throttle);
+
+	void DriveTrack();
 
 	//This is max force per track in newtons
 	UPROPERTY(EditDefaultsOnly, Category = "Setup")
 	float TrackMaxDrivingForce = 400000; //40t tank for 1G acceleration
 	
+	float CurrentThrottle = 0;
+
 };
